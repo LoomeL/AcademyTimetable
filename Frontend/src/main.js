ef208f1a1dc8cb7d4b92fec601d4dc9634fac219
LoomeL <kotgeorgy01@gmail.com>
2024-02-06 13:55:10 +0700

    Шаблон нового ui

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,20 +1,11 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router/router.js'
-
-import '@/bootstrap/bootstrap.scss'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import './fontawesome/css/all.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
-
-import {useThemeStore} from "@/stores/theme.js";
-
-useThemeStore().setTheme()
 
 app.mount('#app')
